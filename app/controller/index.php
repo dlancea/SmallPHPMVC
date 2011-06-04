23fb4c3f0501d2fd767c2f6b1a44cab9e71d08e9
dlancea <dlancea@gmail.com>
2011-06-04 10:41:00 -0700

    Cleaned up comments in example Index Controller

--- app/controller/index.php
+++ app/controller/index.php
@@ -1,16 +1,21 @@
 <?php
 /**
- * Profit Tree controller
- *
- * @package		default
+ * Example Index controller. This controller is loaded by default.
  */
 
 class Controller_Index extends Controller_Base {
+	/**
+	 * Example action. The "index" action is executed by default.
+	 */
 	function index() {
 		$this->registry['template']->show('index');
 	}
 	
+	/**
+	 * Example action with parameters. Parameters are specified in the URL (eg. For "http://local/index/test/123/456", the
+	 * parameter values are "123" for $arg and "456" for $arg2)
+	 */
 	function test($arg = null, $arg2 = null){
-		echo 'this function'.$arg . 'arg2: ' . $arg2;
+		echo 'Test action. $arg: '.$arg . ' arg2: ' . $arg2;
 	}
 }
